a2827c63765360cb542c4abe918df18e2cf0417c
Suvan-L“ <liushuwei0925@gmail.com>
2018-03-26 22:36:56 +0800

    Modify the project *.properties, set Reids and MySQL connection password

--- zheng-common/src/main/java/com/zheng/common/util/AESUtil.java
+++ zheng-common/src/main/java/com/zheng/common/util/AESUtil.java
@@ -129,7 +129,7 @@ public class AESUtil {
 
     public static void main(String[] args) {
         String[] keys = {
-                "", "123456"
+                "", "123456", "liushuwei"
         };
         System.out.println("key | AESEncode | AESDecode");
         for (String key : keys) {
